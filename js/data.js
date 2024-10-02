b8f2f6575ac08ff15c545f69fd45e158015799de
Rolandas J <rolandas.rj@icloud.com>
2024-10-02 20:03:00 +0300

    Created search funcionality (#1)
    * Created search funcionality
    
    * Corrected remarks

--- js/data.js
+++ js/data.js
@@ -68,3 +68,4 @@ export const data = {
 };
 
 data.getTasks();
+
---
+++ js/searchTask.js
@@ -0,0 +1,13 @@
+
+
+function searchTask() {
+  const searchedTask = document.getElementById("seachTaskInput").value;
+
+  if (!searchedTask.trim().length) {
+    alert("Please enter a keyword");
+    document.getElementById("seachTaskInput").value = "";
+    return;
+  }
+}
+
+export { searchTask };
